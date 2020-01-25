5827745e4ec0e948e1ee81c3bfded305109cf795
daha2108 <dv.bogdanova@gmail.com>
2020-01-26 01:01:39 +0200

    create sign up page

---
+++ site_elefanta/js/main.js
@@ -0,0 +1,58 @@
+const login = document.getElementById('login')
+
+const pass1 = document.getElementById('pass1')
+
+const pass2 = document.getElementById('pass2')
+pass2.disable = true
+pass2.placeholder = "repeate password"
+
+const submit = document.getElementById('submit')
+submit.disable = true
+submit.innerText = "submit" 
+
+let hash = ''
+
+pass1.oninput = function (event) {
+  event.target.test = Boolean(event.target.value.match(/\d/)
+      && event.target.value.match(/\w/)
+        && event.target.value.length > 7)
+  console.log(event.target.test)
+  event.target.style.color = event.target.test ? 'green' : 'red'
+}
+
+pass1.onchange = function (event) {
+  if (event.target.test) {
+    pass2.disable = false
+    
+  }
+}
+
+pass2.oninput = function (event) {
+  event.target.style.color = 
+    event.target.value === pass1.value 
+      ? 'green' : 'red'
+}
+
+pass2.onchange = function (event) {
+  if (event.target.value === pass1.value) {
+    hash = Sha256.hash (event.target.value)
+    submit.disable = !(hash && login.value.match(/\S/))
+  }  
+}
+
+submit.onclick = function (event) {
+  fetch (`https://garevna-rest-api.glitch.me/user/${login.value}`,{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        passhash: hash
+    })
+  }).then(response => {console.log (response.ok)
+    if (response.ok) {
+        document.cookie = `login=${login.value}`
+        document.cookie = `pass=${hash}`
+    }
+  })
+}
